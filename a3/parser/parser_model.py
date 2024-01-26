e71ed409f9e87ec0854b248ef709826ec33f287b
Saykin David <saykind@stanford.edu>
2024-01-25 18:27:25 -0800

    Solved a3 q2.

--- a3/parser/parser_model.py
+++ a3/parser/parser_model.py
@@ -73,8 +73,19 @@ class ParserModel(nn.Module):
         ### 
         ### See the PDF for hints.
 
+        self.embed_to_hidden_weight = nn.Parameter(torch.zeros((self.n_features * self.embed_size, self.hidden_size)))
+        nn.init.xavier_uniform_(self.embed_to_hidden_weight)
 
+        self.embed_to_hidden_bias = nn.Parameter(torch.zeros(self.hidden_size))
+        nn.init.uniform_(self.embed_to_hidden_bias)
 
+        self.dropout = nn.Dropout(p=self.dropout_prob)
+
+        self.hidden_to_logits_weight = nn.Parameter(torch.zeros((self.hidden_size, self.n_classes)))
+        nn.init.xavier_uniform_(self.hidden_to_logits_weight)
+
+        self.hidden_to_logits_bias = nn.Parameter(torch.zeros(self.n_classes))
+        nn.init.uniform_(self.hidden_to_logits_bias)
 
         ### END YOUR CODE
 
@@ -107,7 +118,8 @@ class ParserModel(nn.Module):
         ###     View: https://pytorch.org/docs/stable/tensors.html#torch.Tensor.view
         ###     Flatten: https://pytorch.org/docs/stable/generated/torch.flatten.html
 
-
+        x = torch.index_select(self.embeddings, 0, w.flatten())
+        x = x.view(w.shape[0], -1)
 
         ### END YOUR CODE
         return x
@@ -144,6 +156,11 @@ class ParserModel(nn.Module):
         ###     Matrix product: https://pytorch.org/docs/stable/torch.html#torch.matmul
         ###     ReLU: https://pytorch.org/docs/stable/nn.html?highlight=relu#torch.nn.functional.relu
 
+        x = self.embedding_lookup(w)
+        x = torch.matmul(x, self.embed_to_hidden_weight) + self.embed_to_hidden_bias
+        x = F.relu(x)
+        x = self.dropout(x)
+        logits = torch.matmul(x, self.hidden_to_logits_weight) + self.hidden_to_logits_bias
 
         ### END YOUR CODE
         return logits
