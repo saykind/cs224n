a912380c0871330631f0a20789cd2077dbfb9251
Saykin David <saykind@stanford.edu>
2024-02-18 19:03:54 -0800

    Implemented london_baseline.

--- a5/code/src/london_baseline.py
+++ a5/code/src/london_baseline.py
@@ -2,3 +2,23 @@
 #   example in the dev set.
 # Hint: Make use of existing code.
 # Your solution here should only be a few lines.
+
+
+import utils
+from tqdm import tqdm
+
+eval_corpus_path = "birth_dev.tsv"
+
+correct = 0
+total = 0
+
+predictions = []
+for line in tqdm(open(eval_corpus_path, encoding='utf-8')):
+    pred = "London"
+    predictions.append(pred)
+total, correct = utils.evaluate_places(eval_corpus_path, predictions)
+
+if total > 0:
+    print('Correct: {} out of {}: {}%'.format(correct, total, correct/total*100))
+else:
+    print('no targets provided')
